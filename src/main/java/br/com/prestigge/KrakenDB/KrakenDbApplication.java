d4a00023151511839263dba5eeb6031089d0e0d1
Ricardo Borzani <ricardo.borzani@yahoo.com>
2023-05-01 22:16:28 -0300

    Initial setup
    Signed-off-by: Ricardo Borzani <ricardo.borzani@yahoo.com>

---
+++ src/main/java/br/com/prestigge/KrakenDB/KrakenDbApplication.java
@@ -0,0 +1,13 @@
+package br.com.prestigge.KrakenDB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KrakenDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KrakenDbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/prestigge/KrakenDB/KrakenDbApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.prestigge.KrakenDB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KrakenDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
